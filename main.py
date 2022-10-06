ccf444899521d808d78264bcbd07e73de4147de2
Dorcoh4 <33184521+Dorcoh4@users.noreply.github.com>
2022-10-06 13:58:09 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ import datasets
 from transformers import AutoModelForCausalLM, AutoTokenizer
 import torch
 
-model = AutoModelForCausalLM.from_pretrained("facebook/opt-66b", torch_dtype=torch.float16).cuda()
+model = AutoModelForCausalLM.from_pretrained("facebook/opt-6.7b", torch_dtype=torch.float16).cuda()
 
 # the fast tokenizer currently does not work correctly
-tokenizer = AutoTokenizer.from_pretrained("facebook/opt-66b", use_fast=False)
+tokenizer = AutoTokenizer.from_pretrained("facebook/opt-6.7b", use_fast=False)
 dataset = datasets.load_dataset("bookcorpus")
 x = 3
 
