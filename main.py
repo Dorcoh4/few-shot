3fca719f3db0f5f98f220e18d7d0f41acd3b827b
Dorcoh4 <33184521+Dorcoh4@users.noreply.github.com>
2022-10-08 15:50:09 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,35 +8,28 @@ from transformers import AutoModelForCausalLM, AutoTokenizer
 import torch
 
 with torch.no_grad():
-    # accelerator = Accelerator()
     model = AutoModelForCausalLM.from_pretrained("facebook/opt-2.7b", torch_dtype=torch.float16).cuda()
+    accelerator = Accelerator()
     model.eval()
     # the fast tokenizer currently does not work correctly
     tokenizer = AutoTokenizer.from_pretrained("facebook/opt-2.7b", use_fast=False)
     # tokenizer.padding_side = "left"
     dataset = datasets.load_dataset("bookcorpus")['train']
-    batch_size = 1
+    batch_size = 32
+    context_length = 1024
 
+    def tokenize_function(examples):
+        return tokenizer(examples["text"], padding="max_length", truncation=True, max_length=context_length)
 
-    # def tokenize_function(examples):
-    #     input_ids = []
-    #     attention_mask = []
-    #     for example in examples["text"]:
-    #         # print(example)
-    #         encoded_batch = tokenizer.encode_plus(example, return_tensors="pt")
-    #         input_ids.append(encoded_batch['input_ids'].squeeze())
-    #         attention_mask.append(encoded_batch['attention_mask'].squeeze())
-    #     return {'input_ids': input_ids, 'attention_mask': attention_mask}
-
-
-    # tokenized_data = dataset.select(range(100)) #.map(tokenize_function, batched=True)
-    # tokenized_data = tokenized_data.remove_columns("text")
-    dataloader = DataLoader(dataset, shuffle=True, batch_size=batch_size)
+    tokenized_data = dataset.select(range(100)).map(tokenize_function, batched=True)
+    tokenized_data = tokenized_data.remove_columns("text")
+    tokenized_data.set_format("torch")
+    dataloader = DataLoader(tokenized_data, shuffle=True, batch_size=batch_size)
     # perplexity = load("perplexity", module_type="metric")
     losses = []
     for batch in dataloader:
-        batch_encoding = tokenizer(batch['text'], return_tensors="pt")
-        outputs = model(input_ids=batch_encoding['input_ids'].cuda(), labels=batch_encoding['input_ids'].cuda())
+        # batch_encoding = tokenizer(batch['text'], return_tensors="pt")
+        outputs = model(input_ids=batch['input_ids'], labels=batch['input_ids'])
 
         losses.append(outputs.loss.unsqueeze(0))
         x = 3
