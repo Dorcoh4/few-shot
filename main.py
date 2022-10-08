9352ca3eb60352c01d4ad5e10216be2179059cc7
Dorcoh4 <33184521+Dorcoh4@users.noreply.github.com>
2022-10-08 18:13:20 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,30 +8,33 @@ from transformers import AutoModelForCausalLM, AutoTokenizer
 import torch
 
 with torch.no_grad():
-    model = AutoModelForCausalLM.from_pretrained("facebook/opt-2.7b", torch_dtype=torch.float16).cuda()
+    model = AutoModelForCausalLM.from_pretrained("facebook/opt-1.3b", torch_dtype=torch.float16)
     accelerator = Accelerator()
     model.eval()
     # the fast tokenizer currently does not work correctly
-    tokenizer = AutoTokenizer.from_pretrained("facebook/opt-2.7b", use_fast=False)
+    tokenizer = AutoTokenizer.from_pretrained("facebook/opt-1.3b", use_fast=False)
     # tokenizer.padding_side = "left"
     dataset = datasets.load_dataset("bookcorpus")['train']
-    batch_size = 32
-    context_length = 1024
+    batch_size = 1
+    context_length = 512
 
     def tokenize_function(examples):
-        return tokenizer(examples["text"], padding="max_length", truncation=True, max_length=context_length)
+        return tokenizer(examples["text"])
 
-    tokenized_data = dataset.select(range(100)).map(tokenize_function, batched=True)
+    tokenized_data = dataset.map(tokenize_function, batched=True)
     tokenized_data = tokenized_data.remove_columns("text")
     tokenized_data.set_format("torch")
     dataloader = DataLoader(tokenized_data, shuffle=True, batch_size=batch_size)
     # perplexity = load("perplexity", module_type="metric")
     losses = []
+
+    dataloader, model = accelerator.prepare(dataloader, model)
     for batch in dataloader:
         # batch_encoding = tokenizer(batch['text'], return_tensors="pt")
-        outputs = model(input_ids=batch['input_ids'].cuda(), labels=batch['input_ids'].cuda())
-
-        losses.append(outputs.loss)
+        outputs = model(input_ids=batch['input_ids'], labels=batch['input_ids'])
+        all_loss = accelerator.gather_for_metrics((outputs.loss,))
+        for loss in all_loss:
+            losses.append(loss)
         x = 3
     loss = torch.mean(torch.cat(losses))
     try:
