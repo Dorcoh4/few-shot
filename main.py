31f01a14d341830063e6166da9b5ed891a8268f1
Dorcoh4 <33184521+Dorcoh4@users.noreply.github.com>
2022-10-09 14:02:05 +0300

    modularizing

--- main.py
+++ main.py
@@ -1,22 +1,25 @@
+import sys
+
 import transformers
 import datasets
 from evaluate import load
 from torch.utils.data import DataLoader
 from accelerate import Accelerator
+from tqdm import tqdm
 
 from transformers import AutoModelForCausalLM, AutoTokenizer
 import torch
 
-with torch.no_grad():
-    model = AutoModelForCausalLM.from_pretrained("facebook/opt-2.7b", torch_dtype=torch.float16)
-    accelerator = Accelerator()
-    model.eval()
+print(f"device count: {torch.cuda.device_count()}")
+
+model_name = "facebook/opt-1.3b"
+
+def tokenize_data(dataste, tokenizer):
+
     # the fast tokenizer currently does not work correctly
-    tokenizer = AutoTokenizer.from_pretrained("facebook/opt-2.7b", use_fast=False)
     # tokenizer.padding_side = "left"
-    dataset = datasets.load_dataset("bookcorpus")['train']
-    batch_size = 1
-    context_length = 512
+
+    # context_length = 512
 
     def tokenize_function(examples):
         return tokenizer(examples["text"])
@@ -24,17 +27,22 @@ with torch.no_grad():
     tokenized_data = dataset.map(tokenize_function, batched=True)
     tokenized_data = tokenized_data.remove_columns("text")
     tokenized_data.set_format("torch")
-    dataloader = DataLoader(tokenized_data, shuffle=False, batch_size=batch_size)
-    # perplexity = load("perplexity", module_type="metric")
-    losses = []
+    return tokenized_data
 
-    dataloader = accelerator.prepare(dataloader)
-    model = model.to(accelerator.device)
+
+
+def print_quantiles(model, dataloader, accelerator):
+    losses = []
+    dataloader, model = accelerator.prepare(dataloader, model)
+    # model = model.to(accelerator.device)
+    progress_bar = tqdm(range(len(dataloader)))
     for batch in dataloader:
         outputs = model(input_ids=batch['input_ids'], labels=batch['input_ids'])
         all_loss = accelerator.gather_for_metrics((outputs.loss.unsqueeze(0),))
+        print(len(all_loss))
         for loss in all_loss:
             losses.append(loss)
+        progress_bar.update(1)
     res = torch.cat(losses)
     res = res.type(torch.FloatTensor)
     # try:
@@ -59,7 +67,21 @@ with torch.no_grad():
     print(f"quantile 0.95  {torch.quantile(res, 0.95)}")
     print(f"quantile 0.99  {torch.quantile(res, 0.99)}")
 
+def main():
+    with torch.no_grad():
+        accelerator = Accelerator()
+        model = AutoModelForCausalLM.from_pretrained(model_name, torch_dtype=torch.float16)
+        tokenizer = AutoTokenizer.from_pretrained(model_name, use_fast=False)
 
+        model.eval()
+        dataset = datasets.load_dataset("bookcorpus")['train']
+        tokenized_data = tokenize_data(dataset, tokenizer)
+        dataloader = DataLoader(tokenized_data, shuffle=False, batch_size=1)
+        print_quantiles(model, dataloader, accelerator)
+
+
+if __name__ == '__main__':
+    sys.exit(main())
 
 
 
